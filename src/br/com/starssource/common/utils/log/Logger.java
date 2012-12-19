a96d1121363d47305492d39fba5c641bd16d634a
PauloFerreira25 <paulo.ferreira25@gmail.com>
2012-12-19 19:13:07 -0200

    First

---
+++ src/br/com/starssource/common/utils/log/Logger.java
@@ -0,0 +1,84 @@
+package br.com.starssource.common.utils.log;
+
+public class Logger {
+	private String system;
+	private String subsystem;
+	private String metodo;
+
+	public Logger(String system, String subsystem, String metodo) {
+		this.subsystem = subsystem;
+		this.system = system;
+		this.metodo = metodo;
+	};
+
+	public Logger(String system, String subsystem) {
+		this.subsystem = subsystem;
+		this.system = system;
+	};
+
+	public String getSystem() {
+		return system;
+	}
+
+	public void setSystem(String system) {
+		this.system = system;
+	}
+
+	public String getSubsystem() {
+		return subsystem;
+	}
+
+	public void setSubsystem(String subsystem) {
+		this.subsystem = subsystem;
+	}
+
+	public String getMetodo() {
+		return metodo;
+	}
+
+	public void setMetodo(String metodo) {
+		this.metodo = metodo;
+	}
+
+	public void info(String texto) {
+		this.saidaFormatada(texto, "Info");
+	}
+
+	public void debug(String texto) {
+		this.saidaFormatada(texto, "Debug");
+	}
+
+	public void trace(String texto) {
+		this.saidaFormatada(texto, "Trace");
+	}
+
+	public void error(String texto) {
+		this.saidaFormatada(texto, "Error");
+	}
+
+	public void error(String texto, Exception e) {
+		this.saidaFormatada(texto, "Error", e);
+	}
+
+	public void warning(String texto) {
+		this.saidaFormatada(texto, "Warning");
+	}
+
+	public void critical(String texto) {
+		this.saidaFormatada(texto, "Critical");
+	}
+
+	private void saidaFormatada(String texto, String level, Exception e) {
+		saidaFormatada(texto, level);
+		this.saida(e.getMessage() + " " + e.getStackTrace());
+	}
+
+	private void saidaFormatada(String texto, String level) {
+		this.saida(level + " - " + this.system + " - " + this.subsystem + " - "
+				+ this.metodo + " - " + texto);
+	}
+
+	private void saida(String s) {
+		System.out.println(s);
+	}
+}
